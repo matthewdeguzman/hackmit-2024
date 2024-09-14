e782413e123ddc66374e925b7ba62366ae38250f
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-09-14 15:15:01 -0400

    add clerk

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,5 +1,14 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import UserButton from 'clerk-sveltekit/client/UserButton.svelte';
+	import SignedIn from 'clerk-sveltekit/client/SignedIn.svelte';
+	import SignedOut from 'clerk-sveltekit/client/SignedOut.svelte';
 </script>
 
+<SignedIn>
+	<UserButton afterSignOutUrl="/" />
+</SignedIn>
+<SignedOut>
+	<a href="/sign-in">Sign in</a> <span>|</span> <a href="/sign-up">Sign up</a>
+</SignedOut>
 <slot />
